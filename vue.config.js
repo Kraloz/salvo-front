453ba49752f5d514064d1659129e5e0f36e572e9
Tomás Aprile <tomas.eaprile@gmail.com>
2019-11-02 13:50:00 -0300

    build into static backend directory

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+const path = require("path")
+
 module.exports = {
+  // sets frontend build to static folder in backend
+  outputDir: path.resolve(__dirname, "../backend/src/main/resources/static/web"),
+  // assetsDir: "../../static/SPA"
+
   // proxy all webpack dev-server requests starting with /api
   // to our Spring Boot backend (localhost:8088) using http-proxy-middleware
   // see https://cli.vuejs.org/config/#devserver-proxy
