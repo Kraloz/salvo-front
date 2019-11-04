907168cb8957736cb7506d2820bea08a856e8577
Tomás Aprile <tomas.eaprile@gmail.com>
2019-11-04 13:23:13 -0300

    added custom things tailwind

--- tailwind.js
+++ tailwind.js
@@ -1,6 +1,12 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'primary': '#B04537',
+        'accent': '#37B075',
+        'accent-dark': '#2B8A5C'
+      }
+    }
   },
   variants: {},
   plugins: []
