74bc90e7f68b3c14cf49b3b1bf127dae9c4b7365
Tomás Aprile <tomas.eaprile@gmail.com>
2019-11-05 00:10:49 -0300

    store for salvoes

--- src/store/index.js
+++ src/store/index.js
@@ -6,14 +6,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    nickName: null,
+    player: {
+      id: null,
+      nickName: null
+    },
     gameViews: null,
     gpIndex: null,
     currentGame: null,
   },
   mutations: {
-    SET_NICK_NAME(state, nickName) {
-      state.nickName = nickName
+    SET_PLAYER(state, player) {
+      state.player = player
     },
     SET_GAME_VIEWS(state, status) {
       state.gameViews = status
@@ -26,18 +29,36 @@ export default new Vuex.Store({
     }
   },
   getters: {
+    playerId: ({player}) => {
+      if (player) {
+        return player.id
+      } else {
+        return null
+      }
+    },
+    playerNickName: ({player}) => {
+      if (player) {
+        return player.nickName
+      } else {
+        return null
+      }
+    },
     ships: ({currentGame}) => {
       if (currentGame) {
         return currentGame.ships
       } else {
         return null
       }
+    },
+    salvoes: ({currentGame}) => {
+      if(currentGame) {
+        return currentGame.salvoes
+      } else {
+        return null
+      }
     }
   },
   actions: {
-    setNickname({commit}, payload) {
-      commit('SET_NICK_NAME', payload)
-    },
     setGpIndex({commit}, payload) {
       commit('SET_GP_INDEX', payload)
     },
@@ -48,27 +69,47 @@ export default new Vuex.Store({
       commit('SET_GAME_VIEWS', null)
     },
 
-
+    async login({commit}, payload) {
+      try {
+        const response = await axios.get(`/api/player/${payload}`)
+        const player = {
+          id: response.data.data.id,
+          nickName: payload
+        }
+        commit('SET_PLAYER', player)
+      }
+      catch (error) {
+        if (error.response) {
+          console.error(error.response.data)
+          console.error(error.response.status)
+          console.error(error.response.headers)
+        } else if (error.request) {
+          console.error(error.request)
+        } else {
+          console.error('Error', error.message)
+        }
+        console.error(error)
+      }
+    },
     async fetchGameViews({state, commit}) {
-      /* defensive shield */
-      if(!state.nickName) {
+      if(!state.player) {
         return
       }
-      /* **************** */
+
       try {
-        const response = await axios.get(`/api/player/${state.nickName}/game_views`)
+        const response = await axios.get(`/api/player/${state.player.nickName}/game_views`)
         commit('SET_GAME_VIEWS', response.data)
       } catch (error) {
           if (error.response) {
-            // // console.log(error.response.data)
-            // console.log(error.response.status)
-            // console.log(error.response.headers)
+            console.error(error.response.data)
+            console.error(error.response.status)
+            console.error(error.response.headers)
           } else if (error.request) {
-            // console.log(error.request)
+            console.error(error.request)
           } else {
-            // console.log('Error', error.message)
+            console.error('Error', error.message)
           }
-          // console.log(error)
+          console.error(error)
       }
     },
   }
