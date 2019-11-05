6477be63f18bf8fb5ad55252f964fa0684b81552
Tomás Aprile <tomas.eaprile@gmail.com>
2019-11-05 00:11:16 -0300

    milestone: salvoes

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex-container">
-    <div id="dock">
-      <div id="display">
-        <p>Welcome...</p>
-      </div>
-      <!--The new ships will show up here, waiting for been placed in the grid -->
+    <div id="dock" class="order-none">
+      <div id="display"><p>Welcome...</p></div><!--The new ships will show up here, waiting for been placed in the grid -->
     </div>
-    <div id="grid">
+    <div id="grid" class="order-first">
+      <!--The grid will appear here -->
+    </div>
+    <div id="salvoes" class="order-last">
       <!--The grid will appear here -->
     </div>
   </div>
@@ -14,23 +14,16 @@
 
 <script>
 import * as battleship from '@/assets/modules/battleship.js'
+import { mapGetters } from 'vuex'
 
 export default {
   name: "game",
   mounted() {
-    // Create grid
+    // Creating grids
     battleship.createGrid(11, document.getElementById('grid'), 'ships')
-    
-    if (this.$store.getters.ships) {
-      this.$store.getters.ships.forEach(ship => {
-        battleship.createShips(
-          ship.type.toLowerCase(), 5,
-          this.whatOrientation(ship.locations),
-          document.getElementById(`ships${ship.locations[0]}`),false)
-      })
-    } else {
-      battleship.setDefaultShips()
-    }
+    battleship.createGrid(11, document.getElementById('salvoes'), 'salvoes')
+    this.populateShips()
+    this.populateSalvoes()
   },
   methods: {
     whatOrientation([a, b]) {
@@ -40,8 +33,48 @@ export default {
       else {
         return 'vertical'
       }
+    },
+    populateShips() {
+      if (this.ships) {
+        this.ships.forEach(ship => {
+          battleship.createShips(
+            ship.type.toLowerCase(), 5,
+            this.whatOrientation(ship.locations),
+            document.getElementById(`ships${ship.locations[0]}`), false)
+        })
+      } else {
+        // Default if no ships deployed
+        battleship.setDefaultShips()
+      }
+    },
+    populateSalvoes() {
+      if (this.salvoes) {
+        this.salvoes.forEach(salvo => {
+          if (salvo.player === this.playerId) {
+          // Own salvoes
+            salvo.locations.forEach(shot => {
+              let cell = document.getElementById(`salvoes${shot}`, false)
+              cell.style.backgroundColor = '#1656ee'
+            })
+          } else {
+            // Enemy salvoes
+            salvo.locations.forEach(shot => {
+              let cell = document.getElementById(`ships${shot}`, false)
+              cell.style.backgroundColor = 'red'
+            })
+          }
+        })
+      }
     }
-  }
+
+  },
+  computed: {
+    ...mapGetters([
+      'playerId',
+      'ships',
+      'salvoes',
+    ]),
+  },
 }
 </script>
 
